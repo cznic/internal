4747030f7cf2f4c0a01512b00cd68734b167ac3b
Jan Mercl <0xjnml@gmail.com>
2017-09-05 19:53:58 +0200

    Make truncate compatible with os.File semantics.
    	modified:   file.go

--- file/file.go
+++ file/file.go
@@ -124,11 +124,23 @@ func (f *mem) Truncate(size int64) (err error) {
 	}
 
 	first := size >> f.pgBits
-	if size&int64(f.pgMask) != 0 {
+	if po := size & int64(f.pgMask); po != 0 {
+		if p := f.m[first]; p != nil {
+			b := (*p)[po:]
+			for i := range b {
+				b[i] = 0
+			}
+		}
 		first++
 	}
 	last := f.size >> f.pgBits
-	if f.size&int64(f.pgMask) != 0 {
+	if po := f.size & int64(f.pgMask); po != 0 {
+		if p := f.m[last]; p != nil {
+			b := (*p)[po:]
+			for i := range b {
+				b[i] = 0
+			}
+		}
 		last++
 	}
 	for ; first <= last; first++ {
@@ -323,11 +335,23 @@ func (f *file) Truncate(size int64) (err error) {
 	}
 
 	first := size >> f.pgBits
-	if size&int64(f.pgMask) != 0 {
+	if po := size & int64(f.pgMask); po != 0 {
+		if p := f.m[first]; p != nil {
+			b := p[po:]
+			for i := range b {
+				b[i] = 0
+			}
+		}
 		first++
 	}
 	last := f.size >> f.pgBits
-	if f.size&int64(f.pgMask) != 0 {
+	if po := f.size & int64(f.pgMask); po != 0 {
+		if p := f.m[last]; p != nil {
+			b := p[po:]
+			for i := range b {
+				b[i] = 0
+			}
+		}
 		last++
 	}
 	for ; first <= last; first++ {
